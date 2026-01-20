b8aa3999c0482fbbb17b8886921ad4ee4b504474
Kaden Parker <bobert11@byu.edu>
2026-01-20 11:12:34 -0700

    not working yet, but setting up stucture in piece moves calculator to choose between different move sets based on the type of piece

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -1,3 +1,66 @@
 package chess;
 
 import java.util.Collection;
+import java.util.List;
+
+public class PieceMovesCalculator{
+    private final ChessBoard board;
+    private final ChessPosition myPosition;
+
+    public PieceMovesCalculator(ChessBoard board, ChessPosition myPosition) {
+        this.board = board;
+        this.myPosition = myPosition;
+
+    }
+
+    public Collection<ChessMove> calculateBishopMoves(ChessBoard board, ChessPosition myPosition) {
+
+    }
+
+    public Collection<ChessMove> calculatePawnMoves(ChessBoard board, ChessPosition myPosition) {
+
+    }
+    public Collection<ChessMove> calculateRookMoves(ChessBoard board, ChessPosition myPosition) {
+
+    }
+    public Collection<ChessMove> calculateKingMoves(ChessBoard board, ChessPosition myPosition) {
+
+    }
+    public Collection<ChessMove> calculateQueenMoves(ChessBoard board, ChessPosition myPosition) {
+
+    }
+    public Collection<ChessMove> calculateKnightMoves(ChessBoard board, ChessPosition myPosition) {
+
+    }
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        ChessPiece piece = board.getPiece(myPosition);
+        if (piece.getPieceType() == ChessPiece.PieceType.ROOK) {
+            return calculateRookMoves(ChessBoard board, ChessPosition myPosition);
+        }
+
+        else if (piece.getPieceType() == ChessPiece.PieceType.PAWN) {
+            return calculatePawnMoves(ChessBoard board, ChessPosition myPosition);
+        }
+
+        else if (piece.getPieceType() == ChessPiece.PieceType.KNIGHT) {
+            return calculateKnightMoves(ChessBoard board, ChessPosition myPosition);
+        }
+
+        else if (piece.getPieceType() == ChessPiece.PieceType.KING) {
+            return calculateKingMoves(ChessBoard board, ChessPosition myPosition);
+
+        }
+
+        else if (piece.getPieceType() == ChessPiece.PieceType.QUEEN) {
+            return calculateQueenMoves(ChessBoard board, ChessPosition myPosition);
+        }
+
+        else if (piece.getPieceType() == ChessPiece.PieceType.BISHOP) {
+            return calculateBishopMoves(ChessBoard board, ChessPosition myPosition);
+        }
+
+
+    }
+
+
+}
