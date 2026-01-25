3e043206a87ca4e60acd909b0218e22134fde703
Kaden Parker <bobert11@byu.edu>
2026-01-25 10:54:55 -0700

    Updated and cleaned up code

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -67,11 +67,7 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-//        ChessPiece piece = board.getPiece(myPosition);
         PieceMovesCalculator calc = new PieceMovesCalculator(board, myPosition);
-//        if (piece.getPieceType() == PieceType.BISHOP) {
-//            return List.of(new ChessMove(new ChessPosition(5,4), new ChessPosition(1,8), null));
-//        }
         return calc.pieceMoves();
     }
 }
