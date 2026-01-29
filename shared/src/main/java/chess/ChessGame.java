65787202e9898d03f45c7134a842b062d468e281
Kaden Parker <bobert11@byu.edu>
2026-01-29 15:16:15 -0700

    Same logic in check mate for isinstalemate except checking to see if in check first

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -221,8 +221,11 @@ public class ChessGame {
         else {
             for (int i = 1; i <= 8; i++) {
                 for (int j = 1; j<=8; j++) {
-                    if (validMoves(new ChessPosition(i, j)) != null) {
-                        return false;
+                    chess.ChessPiece piece = board.getPiece(new ChessPosition(i, j));
+                    if (piece != null && piece.getTeamColor() == teamColor){
+                        if (!validMoves(new ChessPosition(i, j) ).isEmpty()){
+                            return false;
+                        }
 
                     }
                 }
