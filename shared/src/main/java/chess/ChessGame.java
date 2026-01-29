ef9fc4779fd6ffebc732e3477ca07e1d748a9c56
Kaden Parker <bobert11@byu.edu>
2026-01-29 13:34:51 -0700

    I believe my logic here works for isinstalemate. I check if in check if so return false, else i loop over every position and check for those moves if it returns null, loop keeps iterating and checking if the moves would be null or not

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -113,7 +113,23 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+
+        if (isInCheck(teamColor)) {
+            return false;
+        }
+        else {
+            for (int i = 1; i <= 8; i++) {
+                for (int j = 1; j<=8; j++) {
+                    if (validMoves(new ChessPosition(i, j)) != null) {
+                        return false;
+
+                    }
+                }
+            }
+            return true;
+        }
+
+
     }
 
     /**
