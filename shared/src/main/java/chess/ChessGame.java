b947f893b4b247e4bab7d8789f412d00055a13de
Kaden Parker <bobert11@byu.edu>
2026-01-29 14:33:22 -0700

    Big change. Added the Inincheck method. I use same loop as in isinstalemates, and grab the king. then i loop over all pieces check if enemy piece, then cehck all of their potentially valid moves, if the king is one of them i return true, else i erturn false

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.*;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -51,6 +52,7 @@ public class ChessGame {
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
         ChessPiece piece = board.getPiece(startPosition);
+        TeamColor color = piece.getTeamColor();
         if (piece == null) {
             return null;
         }
@@ -62,7 +64,9 @@ public class ChessGame {
             ChessBoard copy = copyBoard(board);
             fakeGame.setBoard(copy);
             fakeGame.applyMoveHelperFunction(copy, individual_move);
-            if (fakeGame.isInCheck())
+            if (!fakeGame.isInCheck(color)){
+                legal_moves.add(individual_move);
+            }
         }
 
         return legal_moves;
@@ -74,16 +78,16 @@ public class ChessGame {
      * @param move chess move to perform
      * @throws InvalidMoveException if move is invalid
      */
-    public void makeMove(ChessMove move)  {
+    public void makeMove(ChessMove move) throws InvalidMoveException {
         ChessPosition start = move.getStartPosition();
         ChessPosition end = move.getEndPosition();
         ChessPiece piece = board.getPiece(start);
-        board.addPiece(end, piece);
         Collection<ChessMove> legalMoves = validMoves(start);
         if (legalMoves == null | !legalMoves.contains(move)){
-            throw new InvalidMoveException("Move is Not Valid")
+            throw new InvalidMoveException("Move is Not Valid");
         }
-        board.
+        board.addPiece(end, piece);
+        board.addPiece(start, null);
     }
 
     /**
@@ -93,7 +97,38 @@ public class ChessGame {
      * @return True if the specified team is in check
      */
     public boolean isInCheck(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        ChessPosition king_position = null;
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 1; j<=8; j++) {
+                chess.ChessPiece piece = board.getPiece(new ChessPosition(i, j));
+                if (piece != null && piece.getPieceType() == ChessPiece.PieceType.KING && piece.getTeamColor()==teamColor){
+                    king_position = new ChessPosition(i, j);
+                }
+            }
+            }
+
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 1; j<=8; j++) {
+                ChessPosition checkPosition = new ChessPosition(i, j);
+                chess.ChessPiece piece = board.getPiece(new ChessPosition(i, j));
+                TeamColor color = piece.getTeamColor();
+                if (color != null && color != teamColor){
+                    Collection<ChessMove> moves = piece.pieceMoves(board, checkPosition);
+                    Set<ChessPosition> endPositionSet = new HashSet<>();
+                    for (ChessMove move : moves) {
+                        endPositionSet.add(move.getEndPosition());
+                    }
+                    if (endPositionSet.contains(checkPosition)){
+                        return true;
+                    }
+
+                }
+            }
+        }
+
+
+
+        return false;
     }
 
     /**
