618d1658788d0054c42ae0e5f0d2355eaf027c6f
Kaden Parker <bobert11@byu.edu>
2026-02-27 10:56:46 -0700

    Accidentaly in past commit messed up USerDao, grabbed the code from previous commit and have updated. pushing this

--- server/src/main/java/dataaccess/UserDao.java
+++ server/src/main/java/dataaccess/UserDao.java
@@ -1,27 +1,9 @@
 package dataaccess;
 
-import model.GameData;
 import model.UserData;
-
-import java.util.HashMap;
 
 public interface UserDao {
     UserData getUser(String username) throws DataAccessException;
-    void createUser(UserData userData) throws DataAccessException;
+    void createUser(String username) throws DataAccessException;
     void clearUsers() throws DataAccessException;
-
-    public class MemoryGameDao implements GameDao {
-        final private HashMap<Integer, GameData> games = new HashMap<>();
-
-        public void clearGames() {
-            Games.clear();
-        }
-
-        public void updateGame(int gameID, String playerColor, String username) {
-        GameData currentGame = games.get(gameID);
-        if (playerColor == "WHITE") {
-
-        }
-        }
-        }
 }
