0932d2f3830edb7ea4cdbca568d2d6333af0dd5f
Kaden Parker <bobert11@byu.edu>
2026-03-10 11:34:26 -0600

    Made empty files for SQL auth tests and game tests and wrote full test cases for user tests

---
+++ server/src/test/java/passoff/server/SQLUserTests.java
@@ -0,0 +1,58 @@
+package passoff.server;
+
+import dataaccess.DataAccessException;
+import dataaccess.DatabaseManager;
+import dataaccess.SQLUserDao;
+import model.UserData;
+import org.eclipse.jetty.server.Authentication;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SQLUserTests {
+    private SQLUserDao userDao;
+    private DatabaseManager manager;
+
+    @BeforeEach
+    public void setup() throws Exception{
+        manager = new DatabaseManager();
+        userDao = new SQLUserDao(manager);
+        userDao.clearUsers();
+    }
+
+
+    @Test
+    @DisplayName("Create User - Positive")
+    public void createUserPositive () throws DataAccessException{
+        UserData user  = new UserData("kaden", "password", "coolguy@gmail.com");
+        Assertions.assertDoesNotThrow(() -> userDao.createUser(user));
+        UserData retrieved = userDao.getUser("kaden");
+        Assertions.assertNotNull(retrieved);
+        Assertions.assertEquals("kaden", retrieved.username());
+    }
+
+    @Test
+    @DisplayName("Create User - Negative")
+    public void createUserNegative () throws DataAccessException{
+        UserData user  = new UserData("kaden", "password", "coolguy@gmail.com");
+        userDao.createUser(user);
+        Assertions.assertThrows(DataAccessException.class, () -> userDao.createUser(user));
+    }
+
+    @Test
+    @DisplayName("Get User - Positive")
+    public void getUserPositive() throws DataAccessException {
+        userDao.createUser(new UserData("kaden", "brown", "brown@town.com"));
+        UserData user = userDao.getUser("Ash");
+        Assertions.assertNotNull(user);
+    }
+
+    @Test
+    @DisplayName("Get User - Negative")
+    public void getUserNegative() throws DataAccessException {
+        UserData user = userDao.getUser("Random Guy");
+        Assertions.assertNull(user, "Should be null, Random Guy is not in database");
+    }
+
+}
