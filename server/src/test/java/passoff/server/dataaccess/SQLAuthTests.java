16d3cbf992419d473550085a0ecea05b9e0c0232
Kaden Parker <bobert11@byu.edu>
2026-03-10 14:10:13 -0600

    Finished auth test file and pushing

--- server/src/test/java/passoff/server/dataaccess/SQLAuthTests.java
+++ server/src/test/java/passoff/server/dataaccess/SQLAuthTests.java
@@ -1,8 +1,11 @@
 package passoff.server.dataaccess;
 
+import dataaccess.DataAccessException;
 import dataaccess.DatabaseManager;
 import dataaccess.SQLAuthDao;
 import dataaccess.SQLGameDao;
+import model.AuthData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,38 +23,49 @@ public class SQLAuthTests {
 
     @Test
     @DisplayName("Create Auth Success")
-    public void createAuthSuccess() {
-
+    public void createAuthSuccess() throws DataAccessException {
+        AuthData auth = authDao.createAuth("kaden");
+        Assertions.assertNotNull(auth.authToken());
+        Assertions.assertEquals("kaden", auth.username());
     }
 
     @Test
     @DisplayName("Create Auth Bad Request")
     public void createAuthBadRequest() {
-
+        Assertions.assertThrows(DataAccessException.class,
+                () -> authDao.createAuth(null));
     }
 
     @Test
     @DisplayName("Get Auth Success")
-    public void getAuthSuccess() {
-
+    public void getAuthSuccess() throws DataAccessException {
+        AuthData created = authDao.createAuth("kaden");
+        AuthData retrieved = authDao.getAuth(created.authToken());
+        Assertions.assertNotNull(retrieved);
+        Assertions.assertEquals(created.username(), retrieved.username());
+        Assertions.assertEquals(created.authToken(), retrieved.authToken());
     }
 
     @Test
     @DisplayName("Get Auth Bad Request")
-    public void getAuthBadRequest() {
-
+    public void getAuthBadRequest() throws DataAccessException {
+        AuthData retrieved = authDao.getAuth("fake-token-1eueiuieiuei23");
+        Assertions.assertNull(retrieved, "Should return null for non-existent token");
     }
 
     @Test
     @DisplayName("Delete Auth Success")
-    public void deleteAuthSuccess() {
-
+    public void deleteAuthSuccess() throws DataAccessException {
+        AuthData auth = authDao.createAuth("kaden");
+        authDao.deleteAuth(auth.authToken());
+        AuthData retrieved = authDao.getAuth(auth.authToken());
+        Assertions.assertNull(retrieved, "Token should be null after deletion");
     }
 
     @Test
     @DisplayName("Delete Auth Bad Request")
     public void deleteAuthBadRequest() {
-
+        Assertions.assertDoesNotThrow(() -> authDao.deleteAuth("non-existent-token"));
     }
 
 
