f23aea767483e2786f9c4b02483827d7ce3a49e1
Kaden Parker <bobert11@byu.edu>
2026-03-21 12:15:29 -0600

    Updated pom.xml to allow me to use register and result dependencies.

---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,15 @@
+package client;
+import server.request.LoginRequest;
+import server.request.RegisterRequest;
+import server.result.LoginResult;
+import server.result.RegisterResult;
+
+public class ServerFacade {
+    public RegisterResult register(RegisterRequest request) {
+    }
+
+    public LoginResult login(LoginRequest request) {
+
+    }
+
+}
--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,4 +1,6 @@
 package client;
 
 public class ServerFacadeTests {
+
+
 }
