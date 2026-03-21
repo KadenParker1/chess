1c6d9c75a235cc0fb44b134735500d955a4a4c6e
Kaden Parker <bobert11@byu.edu>
2026-03-21 13:52:10 -0600

    Following petshop heavily here. Added states and escape sequences, am updating ChessClient to switch between three different repl loops in UI which will access the serverFacade and thus the server

---
+++ client/src/main/java/client/ChessClient.java
@@ -0,0 +1,51 @@
+package client;
+import java.util.Arrays;
+import java.util.Scanner;
+import static client.EscapeSequences.*;
+
+public class ChessClient {
+    private final ServerFacade server;
+    private final String serverUrl;
+    private State state = State.SIGNEDOUT;
+
+    public PetClient(String serverUrl) throws exception.ResponseException {
+        this.serverUrl = serverUrl;
+        server = new ServerFacade(serverUrl);
+    }
+
+    public void run() {
+        System.out.println("Welcome to Kaden's 240 chess. Type Help to get started.");
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")){
+            printPrompt();
+            String line = scanner.nextLine();
+            try {
+                result = eval(line);
+                System.out.print();
+
+            }
+            catch (Throwable e) {
+                System.out.print( + e.getMessage());
+            }
+        }
+
+    }
+
+    public String eval(String input){
+        var tokens = input.toLowerCase().split(" ");
+        var cmd  = (tokens.length > 0) ? tokens[0] : "help";
+        var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+        return switch (state) {
+            case SIGNEDOUT -> new PreLoginUI(server, this).eval(cmd, params);
+            case SIGNEDIN -> new PostLoginUI(server, this).eval(cmd, params);
+            case IN_GAME -> new GameUI(server, this).eval(cmd, params);
+        };
+    }
+    private void printPrompt() {
+        System.out.print("\n" + RESET + ">>> " + GREEN);
+    }
+
+
+
+}
---
+++ client/src/main/java/client/State.java
@@ -0,0 +1,9 @@
+package client;
+
+public class State package client;
+
+public enum State {
+    SIGNEDOUT,
+    SIGNEDIN,
+    INGAME
+}
